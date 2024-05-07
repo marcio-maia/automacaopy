5a852f127b73bb859c86a45d957a44ea8de0cc0d
marcio-maia <marcio_greg@hotmail.com>
2024-05-07 16:53:28 -0300

    Abrindo o site de cadastro

--- codigo.py
+++ codigo.py
@@ -1,14 +1,36 @@
 # Passo a passo do sistema
 #   Bibliotacas:
-#       pyautogui
+#       pyautogui, time
 import pyautogui
+import time
 
 #   pyautogui.click -> Clicar com o mouse
 #   pyautogui.write -> Escrever um texto
 #   pyautogui.press -> Pressionar uma tecla do windows
+#   pyautogui.hotkey -> apertar um conjundo de teclas (Ctrl C, Ctrl V, Alt tab)
 
 #   1. Abrir o sistema da empresa
+
 #       Abrir o navegardor (chrome)
+pyautogui.press("win")
+pyautogui.write("chrome")
+pyautogui.press("enter")
+time.sleep(3)
+pyautogui.press("tab")
+pyautogui.press("enter")
+pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
+pyautogui.press('enter')
+time.sleep(2)
+pyautogui.click(x=716, y=370)
+pyautogui.write("pythonimpressionador@gmail.com")
+pyautogui.press("tab")
+pyautogui.write("123456")
+pyautogui.press("tab")
+pyautogui.press("enter")
+
+#pyautogui.press("tab")
+#print(pyautogui.position())
+
 #       Entrar no sistema da empresa
 #       https://dlp.hashtagtreinamentos.com/python/intensivao/login     
 #   2. Fazer login
