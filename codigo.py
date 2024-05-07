da23b68a44c63a96abf8531eadc9f78d4687d658
marcio-maia <marcio_greg@hotmail.com>
2024-05-07 16:15:06 -0300

    primeiro commit

---
+++ codigo.py
@@ -0,0 +1,10 @@
+# Passo a passo do sistema
+#   1. Abrir o sistema da empresa
+#       Abrir o navegardor (chrome)
+#       Entrar no sistema da empresa
+#       https://dlp.hashtagtreinamentos.com/python/intensivao/login     
+#   2. Fazer login
+#   3. Abrir/importar a base de dados de produtos para cadastrar
+#   4. Cadastrar um produto
+#   5. repetir isso tudo até acabar a lista de produtos
+
